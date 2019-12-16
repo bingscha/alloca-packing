c6e7862771787a36f2f2fc186e4a66b75beea1db
bingscha <bingscha@umich.edu>
2019-12-16 18:28:12 -0500

    Summation

--- tests/src/test_summation.c
+++ tests/src/test_summation.c
@@ -1,8 +1,10 @@
+#include <stdio.h>
+
 int main() {
     short a = 1, b = 2, c = 3, d = 4;
     long sum = 0;
 
-    for (int i = 0; i < 1000; ++i) {
+    for (int i = 0; i < 100000; ++i) {
         int to_add = 0;
         to_add += a++;
         to_add += b++;
