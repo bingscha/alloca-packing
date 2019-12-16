d09341bfa6c1996961d7e9a7009dd321162cb750
bingscha <bingscha@umich.edu>
2019-12-16 18:21:05 -0500

    added new test

---
+++ tests/src/test_summation.c
@@ -0,0 +1,15 @@
+int main() {
+    short a = 1, b = 2, c = 3, d = 4;
+    long sum = 0;
+
+    for (int i = 0; i < 1000; ++i) {
+        int to_add = 0;
+        to_add += a++;
+        to_add += b++;
+        to_add += c++;
+        to_add += d++;
+
+        sum += to_add;
+    }
+    printf("%ld\n", sum);
+}
