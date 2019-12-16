d1ec2387b6b9fcdf5b7e145aae1a8f562b4c618f
bingscha <bingscha@umich.edu>
2019-12-16 18:33:00 -0500

    Updated summation

--- tests/src/test_summation.c
+++ tests/src/test_summation.c
@@ -4,7 +4,7 @@ int main() {
     short a = 1, b = 2, c = 3, d = 4;
     long sum = 0;
 
-    for (int i = 0; i < 10000000; ++i) {
+    for (int i = 0; i < 100000000; ++i) {
         int to_add = 0;
         to_add += a++;
         to_add += b++;
